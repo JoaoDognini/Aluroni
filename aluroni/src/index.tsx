9163866d745f15f7cdf5d5bf215012e0d880c83a
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2023-09-19 13:35:03 -0300

    Aula 2 - Assets
    Criando assets, adicionando e importando a logo, configuração do absolute imports e reset css.

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Botao from './components/Botao';
+import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <>
-    <Botao />
+    <Cardapio />
     </>
   </React.StrictMode>
 );
--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -1,7 +1,10 @@
 import style from './Cardapio.module.scss'
+import logo from 'assets/logo.svg'
 
 export default function Cardapio() {
     return (
-        <nav className={style.menu}></nav>
+        <nav className={style.menu}>
+            <img src={logo} alt='Logo Aluroni' />
+        </nav>
     )
 }
